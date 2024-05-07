a4ffbe0ca41781f3aaed8f125d8aca91f97764e5
changswon <changs1505@gmail.com>
2024-05-07 16:57:06 +0900

    0507

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:note/constants.dart';
 import 'package:note/screens/jigoo/jigoo.dart';
 import 'package:note/screens/palgaklist/palgaklist.dart';
 import 'package:note/screens/home/searchresultscreen.dart';
 import 'package:note/screens/home/councilscreen.dart';
+import 'package:note/screens/rules/RulesScreen.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -15,6 +17,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: buildAppBar(),
       body: RefreshIndicator(
         // 당겨서 새로고침이 발생할 때 수행할 작업을 정의
@@ -25,21 +28,42 @@ class HomeScreen extends StatelessWidget {
         child: BodyScreen(),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 선택되지 않은 아이템의 색상 모두 출력
+        selectedItemColor: Colors.black, // 선택된 아이템의 아이콘 및 텍스트 색상
+        unselectedItemColor: Colors.black, // 선택되지 않은 아이템의 아이콘 및 텍스트 색상
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: '홈',
           ),
           BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            label: '협의회',
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.notifications),
-            label: '개인정보처리방침',
+            label: '회칙',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: '규정&규약',
           ),
         ],
         onTap: (index) {
-          if (index == 1) {
+          if (index == 0) {
+            Navigator.push(
+              context, // 위젯의 위치, 즉 홈스크린을 가리킴
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          } else if (index == 1) {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => CouncilScreen()),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RulesScreen()),
             );
           }
         },
@@ -58,8 +82,8 @@ class HomeScreen extends StatelessWidget {
             child: Row(
               children: [
                 Image.asset(
-                  'images/18_logo.png',
-                  height: 60.0,
+                  'images/17_logo2.png',
+                  height: 50.0,
                   width: 270.0,
                 ),
               ],
@@ -119,67 +143,131 @@ class BodyScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  SizedBox(width: 10),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => JigooScreen()),
-                      );
-                    },
-                    child: Container(
-                      height: 120,
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          '지구임원단',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => JigooScreen()),
+                          );
+                        },
+                        child: Container(
+                          height: 120,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '역대 연합회장 연혁',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ContentListScreen()),
-                      );
-                    },
-                    child: Container(
-                      height: 120,
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.blueAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text(
-                          '팔각회 목록',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => ContentListScreen()),
+                          );
+                        },
+                        child: Container(
+                          height: 120,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '공조회 회칙',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                  SizedBox(width: 10),
+                  SizedBox(height: 30,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Handle the tap for '회원명부'
+                        },
+                        child: Container(
+                          height: 120,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '회원명부',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => ContentListScreen()),
+                          );
+                        },
+                        child: Container(
+                          height: 120,
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '조선사업부',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
                 ],
               ),
+
               Image.asset(
                 'images/gana.jpg',
                 height: 150.0,
---
+++ lib/screens/rules/RulesScreen.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+class RulesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('회칙'),
+      ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '현.중(주) 사내협력회사 협의회 회칙',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '제1장 총 칙',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '1. 수집하는 개인정보 항목',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+                '회사는 견적문의 등을 위해 아래와 같은 개인정보를 수집하고 있습니다.\n'
+                    '\n'
+                    '수집항목 : 이름, 이메일, 회사명, 부서, 전화번호, 소속, 직위, 시/도'
+
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '2. 개인정보의 수집 및 이용목적',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+                '회사는 수집한 개인정보를 회원목록 제공을 위해 활용합니다.\n'
+                    '-회원수첩 회원목록 제공'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '3. 개인정보의 보유 및 이용기간',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+                '회사는 개인정보 수집 및 이용목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '4. 개인정보의 파기절차 및 방법',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+                '회사는 원칙적으로 개인정보 수집 및 이용목적이 달성된 후에는 해당 정보를 지체없이 파기합니다. 파기절차 및 방법은 다음과 같습니다.\n'
+                    '-전자적 파일형태로 저장된 개인정보는 기록을 재생할 수 없는 기술적 방법을 사용하여 삭제합니다.\n'
+                    '-종이에 출력된 개인정보는 분쇄기로 분쇄하거나 소각을 통하여 파기합니다.'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '5. 개인정보 제공',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+                '회사는 이용자의 개인정보를 원칙적으로 외부에 제공하지 않습니다. 다만, 아래의 경우에는 예외로 합니다.\n'
+                    '이용자들이 사전에 동의한 경우 법령의 규정에 의거하거나, 수사 목적으로 법령에 정해진 절차와 방법에 따라 수사기관의 요구가 있는 경우'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '6. 수집한 개인정보의 위탁',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+                '회사는 고객님의 동의없이 고객님의 정보를 외부 업체에 위탁하지 않습니다.'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '7. 이용자 및 법정대리인의 권리와 그 행사방법',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+                '이용자는 언제든지 등록되어 있는 자신의 개인정보를 조회하거나 수정할 수 있으며, 개인정보관리책임자에게 서면, 전화 또는 이메일로 연락하시면 지체없이 조치하겠습니다.\n'
+                    '귀하가 개인정보의 오류에 대한 정정을 요청하신 경우에는 정정을 완료하기 전까지 당해 개인정보를 이용 또는 제공하지 않습니다. 또한 잘못된 개인정보를 제3자에게 이미 제공한 경우에는 정정 처리결과를 제3자에게 지체없이 통지하여 정정이 이루어지도록 하겠습니다.\n'
+                    '회사는 이용자의 요청에 의해 해지 또는 삭제된 개인정보는 "회사가 수집하는 개인정보의 보유 및 이용기간"에 명시된 바에 따라 처리하고 그 외의 용도로 열람 또는 이용할 수 없도록 처리하고 있습니다.\n'
+                    '만 14세 미만 아동의 경우, 법정대리인이 아동의 개인정보를 조회하거나 수정할 권리, 수집 및 이용 동의를 철회할 권리를 가집니다.'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '8. 개인정보 자동수집 장치의 설치·운영 및 그 거부에 관한 사항',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+                '쿠키 등 인터넷 서비스 이용 시 자동 생성되는 개인정보를 수집하는 장치를 운영하지 않습니다.'
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              '9. 개인정보에 관한 민원서비스',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+                '회사는 고객의 개인정보를 보호하고 개인정보와 관련한 불만을 처리하기 위하여 아래와 같이 관련 부서 및 개인정보관리책임자를 지정하고 있습니다.\n'
+                    '\n'
+                    '-개인정보담당부서 : 가나엔터프라이즈\n'
+                    '-전화번호 : 052-900-7811\n'
+                    '-이 메 일 : design@gana21.com\n'
+                    '\n'
+                    '귀하께서는 회사의 서비스를 이용하시며 발생하는 모든 개인정보보호 관련 민원을 개인정보관리책임자 혹은 담당부서로 신고하실 수 있습니다. 회사는 이용자들의 신고사항에 대해 신속하게 충분한 답변을 드릴 것입니다.\n'
+                    '기타 개인정보침해에 대한 신고나 상담이 필요하신 경우에는 아래 기관에 문의하시기 바랍니다.\n'
+                    '\n'
+                    '1. 개인정보침해신고센터 (http://privacy.kisa.or.kr/118)\n'
+                    '2. 대검찰청 사이버범죄수사단 ( cybercid@spo.go.kr/02-3480-3571)\n'
+                    '3. 경찰청 사이버테러대응센터(http://cyberbureau.police.go.kr /182)'
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
